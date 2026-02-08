a4eae90ddcb974798c51df6e3b71bd38757282fe
Aashish Kumar <kumar.ashish72809@gmail.com>
2026-02-08 14:22:53 +0530

    refactor result proccessors to production-grade

--- sqlc/queries/monitor_incidents.sql
+++ sqlc/queries/monitor_incidents.sql
@@ -6,3 +6,8 @@ VALUES ($1, $2, $3, $4, $5);
 SELECT id, monitor_id, start_time, end_time, alerted, http_status, latency_ms, created_at
 FROM monitor_incidents
 WHERE id = $1;
+
+-- name: CloseMonitorIncident :execrows
+UPDATE monitor_incidents
+SET end_time = $2
+WHERE monitor_id = $1 AND end_time IS NULL;
